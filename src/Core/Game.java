9c41d05fc18f7faf55ac51b61911d739581e1f74
Joseph <g.uydude@yahoo.com>
2018-08-26 20:33:16 -0400

    hope this worked. Replaced all core with copied version of themselves

---
+++ src/Core/Game.java
@@ -0,0 +1,173 @@
+package Core;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import Core.*;
+import Constructs.Block;
+import Constructs.Blocks.ArmorBlock;
+import Constructs.Construct;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.util.ConcurrentModificationException;
+
+/**
+ * this is the part of the screen that you look at while playing and that
+ * contains all gameObjects
+ * @author Joseph
+ */
+public class Game extends Canvas implements Runnable {
+    /*  FIELDS   */
+
+    private Thread thread = null;
+    private boolean running = false;
+    public BufferedImage backgroundImage;
+    public static Handler handler = new Handler();
+    public static int width, height;
+    public Window window;
+    public Input input;
+    public static Game mainGame; //main game instance
+    public static Construct testConstruct;
+    public static Block testBlock;
+    
+    public Game() {
+        mainGame = this;
+        this.width = 700;
+        this.height = 700;
+        window = new Window(this);
+        Setup();
+        input = new Input(this);
+        this.addKeyListener(input);      
+        this.addMouseListener(input);
+    }
+
+    /**
+     * use this method to set starting objects etc
+     */
+    public void Setup() {
+         testConstruct = new Construct(100,100);
+         handler.constructs.add(testConstruct);
+         testBlock = new ArmorBlock();
+         testConstruct.orgin.connect(0, testBlock);
+         testBlock.name = "1";  
+         
+       
+    }
+
+    //core tick, tells all game Objects to tick
+    private void tick() {
+        handler.tick();
+    }
+
+    //core render method, tells all game Objects to render
+    private void render() {
+
+        BufferStrategy bs = this.getBufferStrategy();
+
+        if (bs == null) { ///run once at the start
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g = (Graphics2D)g;
+        Graphics2D g2d = (Graphics2D)g;
+        g.setColor(Color.GREEN);
+
+        this.renderBackGround(g);
+        handler.render(g2d);
+        g.dispose();
+
+        bs.show();
+    }
+
+    /**
+     * renders the background onto the game
+     * @param g 
+     */
+    public void renderBackGround(Graphics g) {
+        try {
+            if (backgroundImage == null) {
+                backgroundImage = ImageIO.read(new File(Main.getDir() + Main.assets + "Platformbg.png"));
+            }
+            g.drawImage(backgroundImage, 0, 0, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Core game loop 
+    @Override
+    public void run() {
+        this.requestFocus(); ///automatically selects window so you dont have to click on it
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (running) {
+                try{
+                this.render();
+                }catch(ConcurrentModificationException cme){
+                    System.out.println("cme render");
+                }
+            }
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+                ///this triggers once a second
+            }
+        }
+        stop();
+    }
+
+    //starts the main game
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    ///stops the main game
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * adds object to the world, the object will be located at whatever x/y coordinates it has
+     * @param o object to add
+     */
+    public void addObject(GameObject o){
+        handler.storage.add(o);
+    }
+    
+    public void removeObject(GameObject o){
+        o.destroy();
+    }
+
+}
---
+++ src/Core/GameObject.java
@@ -0,0 +1,87 @@
+package Core;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import Core.*;
+import java.awt.Graphics2D;
+import java.util.Iterator;
+
+/**
+ * A game object is everything that will go in the game. it ticks (does somthing every frame)
+ * it renders (displays itself) and has coordinates for its X and Y position.
+ * also has an x velocity and a Y velocity variable as well has two arrays for sprites.
+ * those arnt yet implemented however.
+ * @author Joseph
+ */
+public abstract class GameObject {
+    /*  FIELDS  */
+    public int velX, velY, speed;       //X/Y coordinates and X/Y velocity
+    public Coordinate location = new Coordinate(0,0);    //X,Y relative to world
+    public int width, height;                  //length and width
+    public boolean isAlive = true;      
+    public String name;                     //used to identify what kind of gameobject this is
+    /**
+     * constructor that takes two ints for x and y coordinates
+     * @param x
+     * @param y 
+     */
+    public GameObject(int x, int y){
+        location.x = x;
+        location.y = y;
+    }
+    
+    //every game tick, we update the coordinates based on velocity. at the end, clamp the coordinates to within the bounds of the world
+    public void tick(){
+        adjustPositionForVelocity();
+    }
+    
+    public void adjustPositionForVelocity() {
+        for (int i = 0; i < Math.abs(velX); i++) {
+            if (velX < 0) {
+                location.x--;
+            } else if (velX > 0) {
+                location.x++;
+            }
+        }
+        for (int i = 0; i < Math.abs(velY); i++) {
+            if (velY < 0) {
+                location.y--;
+            } else if (velY > 0) {
+                location.y++;
+            }
+        }
+        location.x = Main.clamp(location.x, Game.width - width / 2, 0);
+        location.y = Main.clamp(location.y, Game.height - height / 2, 0);
+    }
+
+    /**
+     * this is run once a frame and contains code that puts the object on
+     * screen.
+     *
+     * @param g
+     */
+    public abstract void render(Graphics2D g);
+    
+
+    /**
+     * removes object from the game handler
+     */
+    public void destroy() {
+        isAlive = false;
+        Iterator<GameObject> it = Game.handler.storage.iterator();
+        while (Game.handler.storage.contains(this)) {
+            try {
+                Game.handler.storage.remove(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("CME from removing");
+            }
+        }
+
+    }
+}
